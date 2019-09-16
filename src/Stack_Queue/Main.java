877a6a71291f1fb3bc8eeb573efbcd258ebde441
lemonlxn <lemonlxn1994@163.com>
2019-09-16 19:45:10 +0800

    修改测试方法

--- src/Stack_Queue/Main.java
+++ src/Stack_Queue/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayStack<Integer> stack = new ArrayStack<>();
 
-        for (int i = 0 ; i < 5; i ++){
+        for (int i = 0 ; i < 10; i ++){
             stack.push(i);
             System.out.println(stack);
         }
