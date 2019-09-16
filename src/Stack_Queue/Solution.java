54c82b5586bec4ac0873aa53b3ba350ecf6bb1f5
lemonlxn <lemonlxn1994@163.com>
2019-09-16 19:47:06 +0800

    使用栈，实现有效字符串匹配

--- src/Stack_Queue/Solution.java
+++ src/Stack_Queue/Solution.java
@@ -66,6 +66,6 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println((new Solution().isValid("{}")));
+        System.out.println((new Solution().isValid("[]")));
     }
 }
